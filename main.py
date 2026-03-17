9d463f6985a70931da0a1eba9d142b5b87cc23af
Aditya kumar <150370020+Adityakumar222@users.noreply.github.com>
2026-03-17 17:20:38 +0530

    main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ Input:
 """
 
     response = requests.post(
-        ""https://router.huggingface.co/hf-inference/models/google/flan-t5-large"",
+        "https://router.huggingface.co/hf-inference/models/google/flan-t5-large",
         headers={
             "Authorization": f"Bearer {HF_API_KEY}"
         },
